3f03eab75ecbdca0ffe593bbea6a6db4c3a1f078
Amom Reis <mirandoamom@gmail.com>
2025-08-13 11:00:14 -0300

    implementando os métodos insert, delete e update

--- src/main/java/com/amomdev/controleDePeso/controllers/ControllerPesagem.java
+++ src/main/java/com/amomdev/controleDePeso/controllers/ControllerPesagem.java
@@ -2,13 +2,13 @@ package com.amomdev.controleDePeso.controllers;
 
 import com.amomdev.controleDePeso.model.Pesagem;
 import com.amomdev.controleDePeso.services.ServicePesagem;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,10 +23,33 @@ public class ControllerPesagem {
         return ResponseEntity.ok(servicePesagem.findAll());
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Pesagem> findById(@PathVariable Long id) {
         return ResponseEntity.ok(servicePesagem.findById(id));
     }
 
+    @PostMapping
+    public ResponseEntity<Pesagem> insert(@RequestBody Pesagem obj) {
+        obj = servicePesagem.insert(obj);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        servicePesagem.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Pesagem> update(@PathVariable Long id, @RequestBody Pesagem obj) {
+        try {
+            var updated = servicePesagem.update(id, obj);
+            return ResponseEntity.ok(updated);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
